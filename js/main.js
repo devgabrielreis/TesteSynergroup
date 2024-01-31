0c073f7e48dda22b3f92ab07aed6e93f2c42ff8b
devgabrielreis <63484148+devgabrielreis@users.noreply.github.com>
2024-01-31 19:51:38 -0300

    Tabela mostra os valores no formato correto

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
-let clientsTable = document.getElementsByClassName("client-list")[0];
+let clientsTableBody = document.getElementsByClassName("clients-table-body")[0];
 let clients = [];
+let currencies = [];
+
+function formatDate(dateStr) {
+    let date = new Date(dateStr);
+
+    let day = (date.getDate() + 1).toString().padStart(2, '0');
+    let month = (date.getMonth() + 1).toString().padStart(2, '0');
+    let year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+}
 
 function loadClientsTable()
 {
@@ -17,23 +28,31 @@ function loadClientsTable()
         row.appendChild(clientNameCell);
 
         let currencyCodeCell = document.createElement("td");
-        currencyCodeCell.textContent = client.currencyCode;
+        currencyCodeCell.textContent = currencies.find((currency) => currency.code === client.currencyCode).abbreviation;
         row.appendChild(currencyCodeCell);
 
         let creationDateCell = document.createElement("td");
-        creationDateCell.textContent = client.creationDate;
+        creationDateCell.textContent = formatDate(client.creationDate);
         row.appendChild(creationDateCell);
 
         let lastSaleDateCell = document.createElement("td");
-        lastSaleDateCell.textContent = client.lastSaleDate;
+        if(client.lastSaleDate === null)
+        {
+            lastSaleDateCell.textContent = "-";
+        }
+        else
+        {
+            lastSaleDateCell.textContent = formatDate(client.lastSaleDate);
+        }
         row.appendChild(lastSaleDateCell);
 
         let totalSalesCell = document.createElement("td");
-        totalSalesCell.textContent = client.totalSales;
+        totalSalesCell.textContent = client.totalSales.toFixed(currencies.find((currency) => currency.code === client.currencyCode).decimalPlaces);
         row.appendChild(totalSalesCell);
 
-        clientsTable.appendChild(row);
+        clientsTableBody.appendChild(row);
     }
+
 }
 
 async function getClients()
@@ -61,9 +80,35 @@ async function getClients()
     }
 }
 
+async function getCurrencies()
+{
+    const url = "/api/currency/get.php";
+
+    try
+    {
+        const response = await fetch(url);
+
+        if(response.status !== 200)
+        {
+            alert("Algo deu errado placeholder");
+            clients = [];
+        }
+        else
+        {
+            const jsonResponse = await response.json();
+            currencies = jsonResponse;
+        }
+    }
+    catch(error)
+    {
+        console.error("Erro no fetch placeholder", error);
+    }
+}
+
 async function intializePage()
 {
     await getClients();
+    await getCurrencies();
     loadClientsTable();
 }
 
